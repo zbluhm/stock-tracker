163f1802fedca3b8acf736f4f4ba6a04e4b29cd4
Zach <zbluhm@live.unc.edu>
2015-02-17 16:25:16 -0500

    testtest

--- src/DbTest.java
+++ src/DbTest.java
@@ -1,6 +1,17 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Created by ZachBluhm on 2/17/15.
  */
 public class DbTest {
+    public static void main(String[]args) {
 
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        System.out.println("Hello");
+    }
 }
