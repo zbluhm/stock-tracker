bab9a9185b3ec8de1848c3acaa0838c7ada7a92a
Zach <zbluhm@live.unc.edu>
2015-02-17 17:16:22 -0500

    mysql test

--- src/DbTest.java
+++ src/DbTest.java
@@ -1,6 +1,10 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
+
+import sun.tools.jconsole.Version;
+
+import java.sql.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 /**
  * Created by ZachBluhm on 2/17/15.
@@ -12,6 +16,37 @@ public class DbTest {
         Statement st = null;
         ResultSet rs = null;
 
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "testuser";
+        String password = "test623";
 
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+
+            }
+        }  catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
     }
 }
